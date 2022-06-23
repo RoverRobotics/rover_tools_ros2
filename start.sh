b3e4a44a4cf820e62efe78d62e7aad72778b80bf
Jack Rivera <jackarivera@gmail.com>
2022-06-23 15:12:50 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt-get update
+sudo apt update
+sudo apt upgrade
 sudo apt-get install python3-pip -y
 sudo apt-get install screen -y
 sudo apt-get install nano -y
@@ -12,9 +13,10 @@ pip install six
 sudo apt update && sudo apt install curl gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update
-sudo apt upgrade
-sudo apt install ros-humble-desktop
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -f
+sudo apt-get install ros-humble-desktop
 source ~/opt/ros/humble/setup.bash
 
 # Rosdep Install
