d4a934277065dc1825f6b288c491f0d546e95fe9
jackarivera <jackarivera@gmail.com>
2022-07-19 11:24:46 -0500

    update commit

---
+++ build/bno055_driver/CMakeFiles/bno055_driver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bno055_driver.dir/src/main.cpp.o"
+  "CMakeFiles/bno055_driver.dir/src/main.cpp.o.d"
+  "bno055_driver"
+  "bno055_driver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bno055_driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
