847cb93111ca85c71bb378a2a17ea714fb850855
jackarivera <jackarivera@gmail.com>
2022-07-27 11:15:26 -0500

    Remove ros2 install to fix source issues

--- start.sh
+++ start.sh
@@ -4,16 +4,14 @@ sudo apt full-upgrade
 sudo apt-get install python3-pip -y
 sudo apt-get install screen -y
 sudo apt-get install nano -y
+sudo apt install net-tools
+sudo apt install cmake
 pip3 install console-menu
 pip3 install boto3
 pip3 install pexpect
 pip install six
 
-# ROS2 Humble Install
-sudo apt update && sudo apt install curl gnupg lsb-release
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt-get install ros-humble-desktop
+# Source ros2
 source ~/opt/ros/humble/setup.bash
 
 # Rosdep Install
