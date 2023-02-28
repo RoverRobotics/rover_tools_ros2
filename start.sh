02206105333006c4eb98e8e937b09bac27e9272c
RoverRoboticsProduction <67167336+RoverRoboticsProduction@users.noreply.github.com>
2023-02-28 15:11:13 -0600

    Update start.sh
    update foxy install and source to resolve colcon build issues

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install curl gnupg2 lsb-release sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo apt install software-properties-common
+sudo add-apt-repository universe
 
-echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main' | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
 sudo apt update && sudo apt upgrade
+sudo apt install ros-foxy-desktop python3-argcomplete
 
-sudo apt install ros-foxy-desktop
-
-sudo apt update
-sudo apt full-upgrade
 sudo apt-get install python3-pip -y
 sudo apt-get install screen -y
 sudo apt-get install nano -y
@@ -32,4 +34,4 @@ python3 ~/rover_tools_ros2/menu/menu_main.py --cf ${cf}
 
 # Source ros2
 source ~/opt/ros/foxy/setup.bash
-source ~/rover_workspace/install/setup.sh
+source ~/rover_tools_ros2/install/setup.sh
