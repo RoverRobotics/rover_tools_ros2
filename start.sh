ddf18c7a6121a0785da3ec9c9597fd75efac7b4a
RoverRoboticsProduction <67167336+RoverRoboticsProduction@users.noreply.github.com>
2023-02-28 14:54:23 -0600

    Update start.sh
    Add ros-foxy-desktop install

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main' | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update && sudo apt upgrade
+
+sudo apt install ros-foxy-desktop
+
 sudo apt update
 sudo apt full-upgrade
 sudo apt-get install python3-pip -y
@@ -12,7 +20,6 @@ pip3 install console-menu
 pip3 install boto3
 pip3 install pexpect
 pip install six
-pip install colcon-common-extensions
 
 # Rosdep Install
 sudo apt install python3-rosdep2
