f7c5c25c4e4aa040cc6b4b9bf7aa1c2ca7593f2a
RoverRoboticsProduction <67167336+RoverRoboticsProduction@users.noreply.github.com>
2023-02-28 15:13:01 -0600

    Update start.sh
    update to resolve source and colcon build issues

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-sudo apt update
-sudo apt full-upgrade
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update && sudo apt upgrade
+sudo apt install ros-foxy-desktop python3-argcomplete
+
 sudo apt-get install python3-pip -y
 sudo apt-get install screen -y
 sudo apt-get install nano -y
